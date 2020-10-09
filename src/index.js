700f259e22514eadc41894848b28f6ea3adf8af7
Tran Thi Hai Ha <tran.thi.hai.ha@sun-asterisk.com>
2020-10-09 15:05:33 +0700

    style: caculator component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import 'antd/dist/antd.css';
 // import * as serviceWorker from './serviceWorker';
 // import App from './App';
 import App from './components/Calculator';
